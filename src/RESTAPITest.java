ba7c79281e961032dcfd88793789ee49832f6946
aishwarya <agbagade20@gmail.com>
2023-01-05 23:22:30 +0530

    my first commit

---
+++ src/RESTAPITest.java
@@ -0,0 +1,20 @@
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	
+	@Test
+	public void postJira() {
+		
+		System.out.println("postJira");
+		System.out.println("postJira1");
+		System.out.println("postJira2");
+		System.out.println("postJira3");
+	}
+	
+	@Test
+	public void deleteTwitter() {
+		
+		System.out.println("deleteTwitter");
+	}
+
+}
---
+++ src/SeleniumTest.java
@@ -0,0 +1,18 @@
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+		
+		@Test
+		public void BrowserAutomation() {
+			
+			System.out.println("Browser Automation");
+			
+		}
+		
+		@Test
+		public void emelemtUi() {
+			
+			System.out.println("elementUi");
+		}
+
+}
